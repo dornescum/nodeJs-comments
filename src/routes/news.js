4888d6ff7c2e95b513454afa76959cdc0983b176
Mihai Dornescu <dornescu@gmail.com>
2021-08-26 18:40:41 +0300

    refacut

--- src/routes/news.js
+++ src/routes/news.js
@@ -23,8 +23,9 @@ newsRouter.get('', async(req, res) => {
         }
     } 
 })
-
+                // pt ca am trecut aici :id( : sunt importante)
 newsRouter.get('/comment/:id', async(req, res) => {
+    // folosesc si aici id
     let articleID = req.params.id;
 
     try {
@@ -47,26 +48,26 @@ newsRouter.get('/comment/:id', async(req, res) => {
 })
 
 
-newsRouter.post('', async(req, res) => {
-    let search = req.body.search
-    try {
-        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts?search=${search}`)
-        res.render('newsSearch', { articles : newsAPI.data })
-    } catch (err) {
-        if(err.response) {
-            res.render('newsSearch', { articles : null })
-            console.log(err.response.data)
-            console.log(err.response.status)
-            console.log(err.response.headers)
-        } else if(err.requiest) {
-            res.render('newsSearch', { articles : null })
-            console.log(err.requiest)
-        } else {
-            res.render('newsSearch', { articles : null })
-            console.error('Error', err.message)
-        }
-    } 
-})
+// newsRouter.post('', async(req, res) => {
+//     let search = req.body.search
+//     try {
+//         const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts?search=${search}`)
+//         res.render('newsSearch', { articles : newsAPI.data })
+//     } catch (err) {
+//         if(err.response) {
+//             res.render('newsSearch', { articles : null })
+//             console.log(err.response.data)
+//             console.log(err.response.status)
+//             console.log(err.response.headers)
+//         } else if(err.requiest) {
+//             res.render('newsSearch', { articles : null })
+//             console.log(err.requiest)
+//         } else {
+//             res.render('newsSearch', { articles : null })
+//             console.error('Error', err.message)
+//         }
+//     }
+// })
 
 
 module.exports = newsRouter 
